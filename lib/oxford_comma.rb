51c96b2a81e36758df08cf0ea69007602b68115a
Anais Momin Ahmed <anaismahmed@uchicago.edu>
2019-07-16 03:38:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+  last = array.pop
+  case array.length
+    when 0
+      return last
+    when 1
+      return "#{array[0]} and #{last}"
+    else
+      "#{array.join(', ')}, and #{last}" 
+  end
 end
